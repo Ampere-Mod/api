a7cb59b7a617e753c268b1b644211bc1e73763d3
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-08 01:18:23 -0400

    Update Constants.kt

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,11 +1,6 @@
 object Ampere {
-  const val VERSION = "0.1.0-alpha.10"
-  const val ID = "ampere"
-  const val DISPLAY_NAME = "Ampere"
+  const val VERSION = "0.1.0-SNAPSHOT"
   const val GROUP = "com.gtlugo.ampere"
-  const val LICENSE = "MIT"
-  const val AUTHORS = "GTLugo"
-  const val DESCRIPTION = "Create power grids to electrify your world."
   const val REPO = "https://github.com/Ampere-Mod/ampere"
   const val PACKAGE = "https://maven.pkg.github.com/Ampere-Mod/ampere"
 }
