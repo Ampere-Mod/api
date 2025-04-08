40b7c26f759fde1b3b05c04ef79e533dde603d42
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-08 01:18:26 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,22 @@ version = Ampere.VERSION
 dependencies {
 
 }
+
+publishing {
+  publications {
+    register<MavenPublication>("mavenJava") {
+      from(components["java"])
+    }
+  }
+
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri(Ampere.PACKAGE)
+      credentials {
+        username = System.getenv("GITHUB_ACTOR")
+        password = System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+}
