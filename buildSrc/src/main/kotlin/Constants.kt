69231d9830cce7c004e85efb9bb1ffd65849cacf
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-10 02:02:01 -0400

    Update Constants.kt

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -2,7 +2,7 @@ object Ampere {
   const val VERSION = "0.1.0-SNAPSHOT"
   const val GROUP = "com.gtlugo.ampere.api"
 
-  private const val PACKAGE = "https://api.ampere-mod.com"
+  private const val PACKAGE = "https://maven.pkg.github.com/ampere-mod/api"
 
   fun packageURL(): String = if (VERSION.endsWith("SNAPSHOT")) {
     "${PACKAGE}/snapshots"
