12722a1df0ae52e90da250a99258de31a556f4a2
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-10 01:54:40 -0400

    Testing maven link

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,29 +1,12 @@
 object Ampere {
   const val VERSION = "0.1.0-SNAPSHOT"
   const val GROUP = "com.gtlugo.ampere.api"
-  const val REPO = "https://github.com/Ampere-Mod/api"
 
-  private const val PACKAGE = "https://maven.pkg.github.com/ampere-mod/api"
+  private const val PACKAGE = "https://api.ampere-mod.com"
 
   fun packageURL(): String = if (VERSION.endsWith("SNAPSHOT")) {
     "${PACKAGE}/snapshots"
   } else {
     PACKAGE
   }
-}
-
-object Parchment {
-  const val MINECRAFT_VERSION = "1.21.1"
-  const val MAPPINGS_VERSION = "2024.11.17"
-}
-
-object Minecraft {
-  const val VERSION = "1.21.1"
-  const val VERSION_RANGE = "[1.21.1]"
-}
-
-object NeoForge {
-  const val VERSION = "21.1.133"
-  const val VERSION_RANGE = "[21.1.133,)"
-  const val LOADER_VERSION = "[1,)"
 }
--- src/main/java/com/gtlugo/ampere/api/AmpereAPI.java
+++ src/main/java/com/gtlugo/ampere/api/AmpereAPI.java
@@ -1,7 +1,10 @@
 package com.gtlugo.ampere.api;
 
-public class AmpereAPI {
+/**
+ * Main class for Ampere's API
+ */
+public final class AmpereAPI {
   public static String bau() {
-    return "bau";
+    return "bau bau";
   }
 }
